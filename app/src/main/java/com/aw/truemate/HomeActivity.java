1c65df6ba062c364a13461de9b776261bac43c69
liorweitz <liorweitz@gmail.com>
2020-10-13 19:45:49 +0300

    added authentication pages

---
+++ app/src/main/java/com/aw/truemate/HomeActivity.java
@@ -0,0 +1,34 @@
+package com.aw.truemate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+
+    Button btnLogout;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        btnLogout = findViewById(R.id.logout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intToMain = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intToMain);
+            }
+        });
+    }
+}
