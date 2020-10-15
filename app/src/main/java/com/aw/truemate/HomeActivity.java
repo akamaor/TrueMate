baad9cd8a77f5b6d2bfce6b36abf4d0399701a72
liorweitz <liorweitz@gmail.com>
2020-10-15 22:24:22 +0300

    homeActivity with photo selection and uploading

--- app/src/main/java/com/aw/truemate/HomeActivity.java
+++ app/src/main/java/com/aw/truemate/HomeActivity.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-//import android.support.v7.app.AppCompatActivity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -17,15 +16,19 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-//this activity folliw the case of successful registraion.
+import java.io.File;
+
+//this activity foll0w the case of successful registraion.
 public class HomeActivity extends AppCompatActivity {
 
     Button btnLogout;
@@ -34,10 +37,11 @@ public class HomeActivity extends AppCompatActivity {
     Button btnMylist;
     private ImageView imageView;
 
-    FirebaseAuth mFirebaseAuth;
-    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private StorageReference mStorageRef;
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    String userId=user.getUid();
 
+    Uri imageUri = null;
     private static final int GALLERY_REQUEST = 9;
 
     @Override
@@ -49,6 +53,8 @@ public class HomeActivity extends AppCompatActivity {
         btnUpdate = findViewById(R.id.updateButton);
         btnMylist=findViewById(R.id.mylistButton);
         imageView=findViewById(R.id.imageView);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
 
  //swiping
         btnSwipe.setOnClickListener(new View.OnClickListener() {
@@ -106,86 +112,42 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        DocumentReference document = FirebaseFirestore.getInstance().document("users" + "/" +documentKey);
-        Uri imageUri = null;
+
         //Check if the intent was to pick image, was successful and an image was picked
         if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK && data != null){
             imageUri = data.getData();
             imageView.setImageURI(imageUri);
-
-            //upload the photo
-
-
+            uploadPhoto(imageUri);
         }
-        if (imageUri != null) {
+   }
+//uploading imageUri to firebase storage. the photo is in dir: images/userId (authentication id)
+    private void uploadPhoto(Uri imageUri) {
 
-            // Code for showing progressDialog while uploading
-            final ProgressDialog progressDialog
-                    = new ProgressDialog(this);
-            progressDialog.setTitle("Uploading...");
-            progressDialog.show();
-
-            // Defining the child of storageReference
-            StorageReference ref
-                    = storageReference
-                    .child(
-                            "images/"
-                                    + UUID.randomUUID().toString());
-
-            // adding listeners on upload
-            // or failure of image
-            ref.putFile(imageUri)
-                    .addOnSuccessListener(
-                            new OnSuccessListener<UploadTask.TaskSnapshot>() {
-
-                                @Override
-                                public void onSuccess(
-                                        UploadTask.TaskSnapshot taskSnapshot)
-                                {
-
-                                    // Image uploaded successfully
-                                    // Dismiss dialog
-                                    progressDialog.dismiss();
-                                    Toast
-                                            .makeText(HomeActivity.this,
-                                                    "Image Uploaded!!",
-                                                    Toast.LENGTH_SHORT)
-                                            .show();
-                                }
-                            })
-
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e)
-                        {
-
-                            // Error, Image not uploaded
-                            progressDialog.dismiss();
-                            Toast
-                                    .makeText(HomeActivity.this,
-                                            "Failed " + e.getMessage(),
-                                            Toast.LENGTH_SHORT)
-                                    .show();
-                        }
-                    })
-                    .addOnProgressListener(
-                            new OnProgressListener<UploadTask.TaskSnapshot>() {
-
-                                // Progress Listener for loading
-                                // percentage on the dialog box
-                                @Override
-                                public void onProgress(
-                                        UploadTask.TaskSnapshot taskSnapshot)
-                                {
-                                    double progress
-                                            = (100.0
-                                            * taskSnapshot.getBytesTransferred()
-                                            / taskSnapshot.getTotalByteCount());
-                                    progressDialog.setMessage(
-                                            "Uploaded "
-                                                    + (int)progress + "%");
-                                }
-                            });
-        }
+        final ProgressDialog pd=new ProgressDialog(this);
+        pd.setTitle("Uploading Image");
+        pd.show();
+        StorageReference riversRef = mStorageRef.child("images/"+userId);
+        riversRef.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        pd.dismiss();
+                        Snackbar.make(findViewById(android.R.id.content), "Image Uploaded", Snackbar.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        pd.dismiss();
+                        Toast.makeText(getApplicationContext(), "Failed to Upload", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                        double progressPercent=(100.00*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+                        pd.setMessage("Percentage: "+(int) progressPercent+"%");
+                    }
+                });
     }
 }
