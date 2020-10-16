813e6dc21a4addfe649daaaa060695d926453e36
meaka <akamaor@gmail.com>
2020-10-16 13:39:15 +0300

    my list without database connection

--- app/src/main/java/com/aw/truemate/HomeActivity.java
+++ app/src/main/java/com/aw/truemate/HomeActivity.java
@@ -61,7 +61,7 @@ public class HomeActivity extends AppCompatActivity {
         btnMylist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(HomeActivity.this, MainActivity.class);
+                Intent i = new Intent(HomeActivity.this, MyListActivity.class);
                 startActivity(i);
             }
         });
