281fbc331eba7d4c2bc2832c29240db1fb741211
liorweitz <liorweitz@gmail.com>
2020-11-02 22:20:20 +0200

    home page upgrade

--- app/src/main/java/com/aw/truemate/MyListActivity.java
+++ app/src/main/java/com/aw/truemate/MyListActivity.java
@@ -21,6 +21,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
